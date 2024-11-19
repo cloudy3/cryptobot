609f10a945884df39ec98f912fb0a1ce1e8d4e24
cloudy3 <cjingfeng98@gmail.com>
2024-11-19 19:17:53 +0800

    fix: store json in artifacts

--- cryto.py
+++ cryto.py
@@ -47,8 +47,8 @@ def track_KID():
     kid_price = kid_price[:8]
 
     # Determine the alert condition
-    if float(kid_price) < 0.0013:
-        message_body = f"KID is below 0.0013, current price: {kid_price}"
+    if float(kid_price) < 0.0009:
+        message_body = f"KID is below 0.0009, current price: {kid_price}"
         save_last_alert_time(current_time)  # Update last alert time
         asyncio.run(send_telegram_message(message_body))
     elif float(kid_price) > 0.002:
